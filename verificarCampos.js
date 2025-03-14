d4422e8ee4388aa024a2ce397eb0fa365863d261
Bruna Diehl <38921507+BrunaDiehl21@users.noreply.github.com>
2025-03-14 16:15:29 -0300

    Add files via upload
    Com verificação aprimorada dos campos e com login pelo Firebase

--- verificarCampos.js
+++ verificarCampos.js
@@ -1,12 +1,28 @@
+//Fazer Login
 const emailUser = document.getElementById('email');
 const senhaUser = document.getElementById('senha');
-const confirmSenhaUser = document.getElementById('confirmSenha');
 const spanDeEmail = document.getElementById('emailSpan');
 const spanDeSenha = document.getElementById('senhaSpan');
+//Criar Conta
+const newEmailUser = document.getElementById('criarEmail');
+const newSenhaUser = document.getElementById('criarSenha');
+const confirmSenhaUser = document.getElementById('confirmSenha');
 const spanDeConfirmSenha = document.getElementById('confirmSenhaSpan');
-const botaoEntrar = document.getElementById('btnEntrar');
+const spanDeConfirmSenha2 = document.getElementById('confirmSenhaSpan2');
+//Botoes
+const botaoEntrarLogin = document.getElementById('btnEntrar1');
+//const botaoEntrarCriarConta = document.getElementById('btnEntrar2');
 
-function verificarInputs(){
+function fazerLogin(){
+    firebase.auth().signInWithEmailAndPassword(emailUser.value, senhaUser.value ).then(response => {
+        window.location.href = "./home.html";
+    }).catch(error => {
+        alert(error.code);
+    });
+}
+
+function verificarInputsLogin(){
+    
     if (emailUser.value.trim() === "") {
         emailUser.classList.remove("corCampoCheio");
         emailUser.classList.add("corCampoVazio");
@@ -27,18 +43,61 @@ function verificarInputs(){
         spanDeSenha.style="visibility: hidden";
     }
 
+}
+
+function verificarInputsCriarConta(){
+    if (newEmailUser.value.trim() === "") {
+        newEmailUser.classList.remove("corCampoCheio");
+        newEmailUser.classList.add("corCampoVazio");
+        spanDeEmail.style="visibility: visible";
+    } else {
+        newEmailUser.classList.add("corCampoCheio");
+        newEmailUser.classList.remove("corCampoVazio");
+        spanDeEmail.style="visibility: hidden";
+    }
+
+    if (newSenhaUser.value.trim() === "") {
+        newSenhaUser.classList.remove("corCampoCheio");
+        newSenhaUser.classList.add("corCampoVazio");
+        spanDeSenha.style="visibility: visible";
+    } else {
+        newSenhaUser.classList.add("corCampoCheio");
+        newSenhaUser.classList.remove("corCampoVazio");
+        spanDeSenha.style="visibility: hidden";
+    }
+
+    
     if (confirmSenhaUser.value.trim() === "") {
         confirmSenhaUser.classList.remove("corCampoCheio");
         confirmSenhaUser.classList.add("corCampoVazio");
         spanDeConfirmSenha.style="visibility: visible";
+        spanDeConfirmSenha2.style="visibility: visible";
     } else {
         confirmSenhaUser.classList.add("corCampoCheio");
         confirmSenhaUser.classList.remove("corCampoVazio");
         spanDeConfirmSenha.style="visibility: hidden";
+        spanDeConfirmSenha2.style="visibility: hidden";
     }
+
+    if (confirmSenhaUser.value !== newSenhaUser.value){
+        confirmSenhaUser.classList.remove("corCampoCheio");
+        confirmSenhaUser.classList.add("corCampoVazio");
+        spanDeConfirmSenha.style="visibility: visible";
+        spanDeConfirmSenha2.style="visibility: visible";
+    }
+    else if (confirmSenhaUser.value === newSenhaUser.value && confirmSenhaUser.value.trim() !== "" ) {
+        confirmSenhaUser.classList.add("corCampoCheio");
+        confirmSenhaUser.classList.remove("corCampoVazio");
+        spanDeConfirmSenha.style="visibility: hidden";
+        spanDeConfirmSenha2.style="visibility: hidden";
+    }
+
 }
 
-botaoEntrar.addEventListener('click', verificarInputs);
+//botaoEntrarLogin.addEventListener('click', verificarInputsLogin);
+
+
+//botaoEntrarCriarConta.addEventListener('click', verificarInputsCriarConta);
 
 form.addEventListener('submit', e => {
     e.preventDefault();
